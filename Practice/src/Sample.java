a886e5333442a0532e012f39f945c158c1c904d2
Lenovo <Lenovo@DESKTOP-TDMCPJD>
2019-11-07 19:39:40 +0530

    First Commit

---
+++ Practice/src/Sample.java
@@ -0,0 +1,40 @@
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int count;
+
+		for (int i = 1; i < 1000; i++) {
+
+			count = 0;
+			for (int j = 2; j < i / 2; j++) {
+
+				if (i % 2 == 0) {
+
+					count = 1;
+
+					break;
+					
+					
+
+				}
+
+			}
+			if (count == 0) {
+				System.out.println(i);
+
+			}
+			
+			if (count == 1) {
+				System.out.println(i +"sadkl");
+
+			}
+
+
+		}
+
+	}
+
+}
